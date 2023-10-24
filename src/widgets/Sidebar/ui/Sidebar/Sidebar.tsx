caeaadab8f1f99e4b2e00588ee28f130afe4ec2a
dmmelnikov <dmitriy.m.gpb@yandex.ru>
2023-10-24 17:06:50 +0300

    Добавил переводы

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,20 +2,19 @@ import React, {useState} from 'react';
 import cls from './Sidebar.module.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
-
-
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
 
 
 interface SidebarProps {
-className?: string;
+    className?: string;
 }
 
 
 export const Sidebar = ({className}: SidebarProps) => {
-const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
-    setCollapsed((prev) => !prev)
+        setCollapsed((prev) => !prev)
     }
 
     return (
@@ -23,7 +22,8 @@ const [collapsed, setCollapsed] = useState(false);
         >
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
-            <ThemeSwitcher />
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
