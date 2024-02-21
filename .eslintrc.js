be7d33a62f2cf6d9fae318bbf94734915da4e506
dmmelnikov <dmitriy.m.gpb@yandex.ru>
2024-02-21 18:06:10 +0300

    добавил prettier, jest и первый тест

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:i18next/recommended",
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:i18next/recommended',
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint",
-        "react",
-        'i18next'
-    ],
-    "rules": {
+    plugins: ['@typescript-eslint', 'react', 'i18next'],
+    rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
         'no-mixed-spaces-and-tabs': 0,
         'react/react-in-jsx-scope': 0,
         'no-unused-vars': 'warn',
-        'i18next/no-literal-string': ['error', {markupOnly:true}],
-    }
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true }],
+    },
 }
